7a4dedaad56d3d39ae2a2e729292992f90aeecf8
laguirre <laguirre@gruposion.bo>
2021-10-26 23:39:55 -0400

    eliminar tweet

---
+++ routers/eliminarTweet.go
@@ -0,0 +1,24 @@
+package routers
+
+import (
+	"net/http"
+
+	"gitgub.com/limbertaguirrerengipo/apigotwittor/bd"
+)
+
+/*EliminarTweet permite borrar un Tweet determinado */
+func EliminarTweet(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro Id", http.StatusBadRequest)
+		return
+	}
+
+	err := bd.BorroTweet(ID, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrio un error al intentar borrar el tweet"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
